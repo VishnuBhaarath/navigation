c35e472da9242273263c7586f98eca38033d136a
VishnuBhaarath <vishnubhaarath30@gmail.com>
2020-03-15 18:28:32 +0530

    Addition of default splash activity using easysplash

--- app/src/main/java/com/example/navigation/Splashscreenactivity.java
+++ app/src/main/java/com/example/navigation/Splashscreenactivity.java
@@ -13,24 +13,14 @@ public class Splashscreenactivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreenactivity);
 
         EasySplashScreen config = new EasySplashScreen(Splashscreenactivity.this)
                 .withFullScreen()
                 .withTargetActivity(MainActivity.class)
-                .withSplashTimeOut(5000)
-                .withBackgroundColor(Color.parseColor("#1a1b29"))
-                .withHeaderText("Header")
-                .withFooterText("Footer")
-                .withBeforeLogoText("Before Logo Text")
-                .withAfterLogoText("After Logo Text")
-                .withLogo(R.mipmap.ic_launcher_round);
+                .withSplashTimeOut(5000);
 
-        config.getHeaderTextView().setTextColor(Color.WHITE);
-        config.getFooterTextView().setTextColor(Color.WHITE);
-        config.getBeforeLogoTextView().setTextColor(Color.WHITE);
-        config.getAfterLogoTextView().setTextColor(Color.WHITE);
-
-        View easySplashScreen = config.create();
-        setContentView(easySplashScreen);
+     
+        setContentView(R.layout.splashscreenactivity);
     }
 }
